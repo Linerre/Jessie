c304ef857e825c33826abc04840f186518ece9be
Linerre <leon.errelin@outlook.com>
2020-02-13 22:19:44 +0800

    worked in Anki; next test functions

--- highlighters/jshl.js
+++ highlighters/jshl.js
@@ -1,5 +1,12 @@
 let code = document.querySelector('code').innerHTML;
-let re = /\/\/.*/g;
-if (re.test(code)) {
-document.querySelector('code').innerHTML= code.replace(re, '<span class="grey">'+'$&'+'</span>');
+let cutter = /.*<br>/g;
+let lines = code.match(cutter);
+
+let comment = /\/\/.*<br>/g;
+
+
+for (let line of lines) {
+    if (comment.test(line)) {
+        document.querySelector('code').innerHTML= code.replace(comment, '<span class="grey">'+line+'</span>');
+    } else continue;
 }
