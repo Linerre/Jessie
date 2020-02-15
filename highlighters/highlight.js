bb74fcc2c108d80f64651bbe422e8c8570c75ddb
Linerre <leon.errelin@outlook.com>
2020-02-15 23:49:25 +0800

    add the main js: highlight.js

---
+++ highlighters/highlight.js
@@ -0,0 +1,18 @@
+// This is the main script that load both the language syntax highlighter and the needed style
+// import the needed modules
+
+import * as sh from "./langs/clhl.js";
+// import * as js from "./jshl.js";
+
+let codeBlock = document.querySelector('code'),
+    cmd = codeBlock.innerHTML;
+
+let lan = document.getElementsByClassName('lang')[0];
+if (lan.textContent == 'Linux') {
+    let newCmd = sh.highlight(cmd);
+    console.log(newCmd);
+    codeBlock.innerHTML = sh.addLineNumber(newCmd);
+}
+//  else if (lan.textContent == 'js') {
+//     js.highlight();
+// }
