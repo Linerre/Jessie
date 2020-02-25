30bc3c7942bb018c47c9f13063370e707459bfc1
Linerre <leon.errelin@outlook.com>
2020-02-25 15:39:12 +0800

    add html highlighter

---
+++ highlighters/langs/htmlhl.js
@@ -0,0 +1,29 @@
+import {STYLES as st} from "./styles.js";
+
+export let
+// tag names: red + cyan
+HTMLTAG = [
+    // begining tag
+    /(&lt;)([a-z]+)([\s\w"=]+)?(&gt;)/,
+
+    //ending tag
+    /(&lt;\/)([a-z]+)(&gt;)/
+]
+
+
+// values
+HTMLGREEN = /"([^"]+)"/;
+
+export function highlight(data) {
+    data = data.replace(new RegExp(HTMLTAG[0], 'g'), 
+    st.CYAN+'$1'+st.CLOSE+
+    st.RED+'$2'+st.CLOSE+
+    '$3'+
+    st.CYAN+'$4'+st.CLOSE);
+
+    data = data.replace(new RegExp(HTMLTAG[1], 'g'),
+    st.CYAN+'$1'+st.CLOSE+
+    st.RED+'$2'+st.CLOSE+
+    st.CYAN+'$3'+st.CLOSE);
+}
+
