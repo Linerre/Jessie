7c0a7c2be2d669a36941cef7696db2a6a1a589f5
Linerre <leon.errelin@outlook.com>
2020-03-27 21:54:45 +0800

    first attemp works

--- gs/gmail/turns-calculator.js
+++ gs/gmail/turns-calculator.js
@@ -4,7 +4,7 @@ to borrow the two books
 */
 
 // Patron
-var PATRONS = {
+const PATRONS = {
   stuA: 'Jerry',
   stuB: 'Zeping',
   stuC: 'Kennedy',
@@ -12,10 +12,16 @@ var PATRONS = {
 };
 
 // Status
-var STATUS = {
+const STATUS = {
   bookA: 'See Yourself Sensing',
   bookB: 'See Yourself X',
   wait : 'Waiting'
+};
+
+// coordinates
+const COOR = {
+	row: 7, // starting point, ever-changing
+	col: 3 //  fixed, unless further adjustment
 };
 
 // algorithm
@@ -23,16 +29,18 @@ function test() {
   // default way of getting the active (woriking) sheet [ATM, subject to change]
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getSheets()[0];
-
-  // Passing only two arguments returns a "range" with a single cell.
-  var range = sheet.getRange('B2');
-  var value = range.getValues();
-
-  // get the patrons and their status in last round
-
-  // loop over them
-  // if status is book a or book b, then waiting
-
+  sheet.appendRow(['Patron', 'Status', 'Last Round']);
+  
+  for (var i = COOR.row; i < COOR.row + 4; i++) {
+  	var patronInfo = sheet.getRange(`A${i}:C${i}`).getValues();
+  	var patronName = patronInfo[0][0];
+  	var patronStat = patronInfo[0][1];
+  	var patronLast = patronInfo[0][2];
+  	// if status is book a or book b, then waiting
+  	if (patronStat == STATUS.bookA || patronStat == STATUS.bookB) {
+  		sheet.appendRow([patronName, 'waiting', patronStat]);
+  	} else continue;
+	}
   // else if status is waiting + book_a (last round), then book_b
 
   // else if status is waiting + book_b (last round), then book_a
