9838e0e8b8da55dbf46ef259bb9c0e949ea205fc
Linerre <leon.errelin@outlook.com>
2020-04-05 14:35:30 +0800

    stand alone script succeeded in accessing ss

--- gsuite/cdl-manager.js
+++ gsuite/cdl-manager.js
@@ -1,16 +1,20 @@
 /* mange CDL */
 
-var folderId = '1SAJvyGFo6tkhJC2w2-qYDirscHdAwVUS'
+var folderId = '1SAJvyGFo6tkhJC2w2-qYDirscHdAwVUS';
+var spreadSheetId = '1hLAlvRtzIgDYjOdrbUZGSqWmVsPPEkddMqZkjLWaKIc';
+var row = 2, colBarcode = 1, colTitle = 2;
 
 
-
-
-
-
-function getEachFileName(folderIdStr) {
+function test() {
 	var folder = DriveApp.getFolderById(folderId);
 	var files = folder.getFiles();
+	var sheet = SpreadsheetApp.openById(spreadSheetId).getSheetByName('reserves');
 	while (files.hasNext()) {
-		var filename = file.next().getName();
+		var filename = files.next().getName();
+		var barcodeCell = sheet.getRange(row, colBarcode);
+		var titleCell = sheet.getRange(row, colTitle);
+		barcodeCell.setValue(filename.slice(0,14));
+		titleCell.setValue(filename.slice(16));
+		row+=1;
 	}
 }
