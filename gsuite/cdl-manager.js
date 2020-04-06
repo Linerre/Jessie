6dbe884ff15f0862c1c050aeb35933f96bd6097f
Linerre <leon.errelin@outlook.com>
2020-04-06 22:01:10 +0800

    self check done

--- gsuite/cdl-manager.js
+++ gsuite/cdl-manager.js
@@ -4,6 +4,21 @@ var folderId = '1SAJvyGFo6tkhJC2w2-qYDirscHdAwVUS';
 var spreadSheetId = '1hLAlvRtzIgDYjOdrbUZGSqWmVsPPEkddMqZkjLWaKIc';
 var row = 2, colBarcode = 1, colTitle = 2;
 
+// 1 sec = 1000 millseconds
+// 4 hours = 4 * 60 * 60 * 1000 = 14400000
+var loanPeriod = 14400000;
+
+function selfCheckout(useremail) {
+	var folder = DriveApp.getFolderById(folderId);
+	var files = folder.getFiles();
+	files.next().addViewer(useremail);
+}
+
+function loanPeriodChecker(loanTime, useremail){
+	// if the item is available
+	selfCheckout(useremail);
+	SpreadsheetApp.openById
+}
 
 function test() {
 	var folder = DriveApp.getFolderById(folderId);
@@ -13,8 +28,8 @@ function test() {
 		var filename = files.next().getName();
 		var barcodeCell = sheet.getRange(row, colBarcode);
 		var titleCell = sheet.getRange(row, colTitle);
-		barcodeCell.setValue(filename.slice(0,14));
-		titleCell.setValue(filename.slice(16));
+		barcodeCell.setValue(filename.slice(0,14)).setWrap(true);
+		titleCell.setValue(filename.slice(15)).setWrap(true);
 		row+=1;
 	}
 }
