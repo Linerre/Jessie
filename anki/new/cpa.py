7c1e01cec9c983705351c8b29ef328ec685495b4
Linerre <leon.errelin@outlook.com>
2020-06-07 17:48:38 +0800

    change a var name

--- anki/new/cpa.py
+++ anki/new/cpa.py
@@ -1,6 +1,6 @@
 #! /bin/env python
 
-# A web scrapper to get exercises questons for CPA
+# A web scrapper for CPA drills
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
@@ -13,21 +13,25 @@ url = 'http://www.zgcjpx.com/cpa/tiku/lianxi/kj/129401.html'
 with urlopen(url) as response:
     html = response.read()
 
+# may consider using strainer to parse only the needed part
 page = soup(html, 'html.parser')
 
-# the <p> tag needed all have one thing in common:
-# they all contain only one child: a textnode
-# so if a <p>'s contents has a length larger than one
-# filter out that annoying <p>, and the rest would be the darling we love
+
+# filter out annoying <p>s, and the rest would be the darling we love
 def drills_para(tag):
     return tag.name == 'p' and len(tag.contents) <= 1 and isinstance(tag.contents[0], str)
 
-test = page.find_all(drills_para)
+drills = page.find_all(drills_para)
 
 # remove unwanted elements
-del test[0]
-del test[-1]
+del drills[0]
+del drills[-1]
+
+
+# figure out how to clean the data
+
+# ---------- cleaning -----------
 
 pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(test)
+pp.pprint(drills)
 
