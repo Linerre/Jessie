eac627ff7de5682682700a85f4786087dbd87315
Linerre <leon.errelin@outlook.com>
2020-06-17 23:58:43 +0800

    combine two funcs into one

--- anki/new/badtogood/para_scanner.py
+++ anki/new/badtogood/para_scanner.py
@@ -5,21 +5,21 @@
 # that surround questions, options, and anwsers.
 import re
 
-# patterns
-correct = re.compile('【答案】([A-D]+)')
-analysis= re.compile('解析：(.*)。')
-chapter = re.compile('\(第.+?章.*?\)')
-q_type  = re.compile('[1-5]\.[\(【]([单多项选择题]+)[\)】](.*?)\((第.+?章.*?)\)')
 
-def normal(list_a, list_b, a_file):
+def normal_old(list_a, list_b, a_file):
     '''
     raw input --> organized outpt for csv files:
     type,Q,chapter_info,optionA,...,A,analysis
     '''
+    answer = r'[1-5\.【参考答案】]+(?P<opt>[A-D]+)[。题目详解析:：]+(?P<aly>.*)'
+    question  = r'[1-5\.\(【]+(?P<type>[单多项选择题]+)[\)】](?P<que>.*?)\((?P<chp>第.+?章.*?)\)'
+    
     # input raw --> [A-D]+,XXX
     for i in range(len(list_b)):
-        list_b[i] = correct.search(list_b[i]).group(1) + ',' \
-                    + analysis.search(list_b[i]).group(1)
+        # get a match
+        a_match = re.search(answer, list_b[i])
+        # combine into one line
+        list_b[i] = a_match.group('opt') + ',' + a_match.group('aly')
         list_a.insert((i+1)*5+i, list_b[i])
     
     # input raw --> [单多选]+,题目,索引
@@ -36,14 +36,17 @@ def normal(list_a, list_b, a_file):
     
     return 'Normal page rendering done!'
 
-def normal_h2(list_a, list_b, q_string, a_file):
+def normal(list_a, list_b, h2, a_file):
     '''
     Append question type (in a <h2> tag) to each Q;
     Output organized lines suitable for csv files
     '''
-    que_type = q_string
-    answer = r'[1-5\.]+[【参考答案】:：]+(?P<opt>[A-D]+)[。【题目详解析:：】]+(?P<aly>.*)'
-    question = r'[1-5\.]+(?P<que>.*)\((?P<chp>第.+?章.*?)\)'
+
+    answer = r'[1-5\.【参考答案】:：]+(?P<opt>[A-D]+)[。【题目详解析:：】]+(?P<aly>.*)'
+
+    question  = r'[1-5\.\(【]+(?P<type>[单多项选择题]+)[\)】](?P<que>.*?)\((?P<chp>第.+?章.*?)\)'
+    question_h2 = r'[1-5\.]+(?P<que>.*)\((?P<chp>第.+?章.*?)\)'
+
     for i in range(len(list_b)):
         # match the needed parts
         a_match = re.search(answer, list_b[i])
@@ -55,14 +58,24 @@ def normal_h2(list_a, list_b, q_string, a_file):
     for i in (0,6,12,18,24):
         # split the questions by matching the needed parts
         try:
-            q_match = re.search(question, list_a[i])
+            # no h2
+            if h2 == None:
+                q_match = re.search(question, list_a[i])
 
-        # combine into a csv and assign to ith question
-            list_a[i] = q_match.group('que') + ',' + q_match.group('chp')
+            # combine into a csv and assign to ith question
+                list_a[i] = q_match.group('type') + ',' + \
+                q_match.group('que') + ',' + \
+                q_match.group('chp')
+            
+            # h2 type 
+            else:
+                q_match = re.search(question_h2, list_a[i])
+                list_a[i] = h2 + ',' + \
+                q_match.group('que') + ',' + \
+                q_match.group('chp')
 
         # write to a csv file
-            a_file.write(que_type + ',' + \
-                    list_a[i] + ',' + \
+            a_file.write(list_a[i] + ',' + \
                     list_a[i+1] + ',' + \
                     list_a[i+2] + ',' + \
                     list_a[i+3] + ',' + \
