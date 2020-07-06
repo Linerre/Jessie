ba515becd08afa087c8b246805faf5e3009cee52
Linerre <leon.errelin@outlook.com>
2020-07-06 14:16:13 +0800

    decorators testing done; not suitable for this porject

--- anki/new/bad_to_good/decorators.py
+++ anki/new/bad_to_good/decorators.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def length_meter(func, a_list):
-	def wrapper():
-		print(f'ATM The {a_list} length is {len(a_list)}')
-		func()
-		print(f'Now the {a_list} length is {len(a_list)}')
+def length_meter(func):
+	def wrapper(*args, **kwargs):
+		print('This block got executed')
+		func(*args, **kwargs)
+		return func(*args,**kwargs)
 	return wrapper
