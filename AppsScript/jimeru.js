be2111662650cfb968634f323cc049349c65047a
Linerre <leon.errelin@outlook.com>
2020-07-07 15:02:47 +0800

    tools updated

--- AppsScript/jimeru.js
+++ AppsScript/jimeru.js
@@ -1,5 +1,6 @@
 // Gmail Cleaner
 
+/* ------------------- Tool 1 ------------------- */ 
 // check labeled threads in the inbox and archive them automatically
 function archiver()
 {
@@ -18,6 +19,8 @@ function archiver()
   }
 }
 
+
+/* ------------------- Tool 2 ------------------- */ 
 // find and clean the "someone mentioned you in a comment" notifications from Google
 function commentsCleaner()
 {
@@ -32,5 +35,36 @@ function commentsCleaner()
     // mark unimportant and throw to trash
     GmailApp.markThreadsUnimportant(threads);
     GmailApp.moveThreadsToTrash(threads);
+  }
+}
+
+/* ------------------- Tool 3 ------------------- */ 
+// create a list of newsletter sources
+const NEWS = [
+  //newletter0,
+  'noreply@mail.bloombergbusiness.com',
+ 
+  // newsletter1,
+  
+  // newsletter2,
+  
+  // ...
+  
+];  
+
+
+function newsCleaner() 
+{
+  for (var news of NEWS)
+  {
+    // find those alraedy read 
+    var threadsRead = GmailApp.search(`from:${news} is:read label:inbox`);
+  
+    // also those unread for one day
+    var threadsUnread = GmailApp.search(`from:${news} is:unread label:inbox older_than:1d`);
+  
+    // Trash them regardless of reading or not
+    GmailApp.moveThreadsToTrash(threadsRead);
+    GmailApp.moveThreadsToTrash(threadsUnread);
   }
 }
